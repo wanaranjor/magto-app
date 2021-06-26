a61d409425418fbce9f2320e1d8267c60e877d8e
Wilson Andres Naranjo Romero <wanaranjo@gmail.com>
2021-06-26 00:50:37 -0500

    [update] nav titles

--- src/lib/shared/ui/Nav.svelte
+++ src/lib/shared/ui/Nav.svelte
@@ -7,25 +7,25 @@
 		class="flex items-center sm:justify-end justify-center px-6 py-3 mx-auto text-gray-600 capitalize dark:text-gray-300"
 	>
 		<a href="/" class="text-gray-800 dark:text-gray-200 border-b-2 border-blue-500 mx-1.5 sm:mx-6"
-			>home</a
+			>option</a
 		>
 
 		<a
 			href="/"
 			class="border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6"
-			>features</a
+			>option</a
 		>
 
 		<a
 			href="/"
 			class="border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6"
-			>pricing</a
+			>option</a
 		>
 
 		<a
 			href="/"
 			class="border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6"
-			>blog</a
+			>option</a
 		>
 
 		<a
